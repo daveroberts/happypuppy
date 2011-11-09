5cca5e1dbeb535520411d80e1f19dde9b696df95
Dave Roberts <Dave.A.Roberts@gd-ais.com>
2011-11-09 10:02:25 -0500

    testing git ignore

--- apps/wiki/views/article/show.php
+++ apps/wiki/views/article/show.php
@@ -2,3 +2,4 @@
 <div><?= $article->getHTML() ?></div>
 <div><?= link_to("Edit", "/article/edit/".$article->slug) ?></div>
 <div><?= link_to("Delete", "/article/delete/".$article->id) ?></div>
+<div>Token Change</div>
